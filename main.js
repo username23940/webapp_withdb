16d531f8aa47e8c5c8234e3a36b97f223ed7da88
LIM JANGBIN <great_jb2005@naver.com>
2025-10-26 09:33:50 +0900

    Enhance topic management with author selection
    Updated SQL queries to include author selection and fixed errors in update process.

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var app = http.createServer(function(request,response){
     
     if(pathname === '/'){
       if(queryData.id === undefined){
-        db.query('SELECT * FROM topic', function(error, topics){ // 콜백함수의 형식(signature). sql의 결과가 topics에 담김
+        db.query('SELECT * FROM topic', function(error, topics){ // 콜백함수의 형식(signature). sql의 결과가 topics에 담김. template.HTML의 매개변수에 들어가야하므로 모든 페이지 반드시 필요    
             if(error) {
                 throw error;
             }
@@ -43,14 +43,14 @@ var app = http.createServer(function(request,response){
               throw error;
               }
           db.query(`SELECT * FROM topic LEFT JOIN author on author.id=topic.author_id WHERE topic.id=${queryData.id}`, function(error2, topic){ // id=?`, [queryData.id] 로 쓰면 sql injection 방어 가능. 두번째 인자가 ?에 치환
-            if(error2) {
+            if(error2) { // LEFT JOIN으로 저자 보기 기능 추가
               throw error2; // 오류가 있다면 알리고 즉시 중지
               }
             var title = topic[0].title; // topic 들고 올 때 배열로 들고옴(단 하나의 요소가 객체. topics와 달리 WHERE로 하나만 선택했으니까)
             var description = topic[0].description;
             var list = template.list(topics);
             var html = template.HTML(title, list,
-              `<h2>${title}</h2>${description}<p>by ${topic[0].name}</p>`, // JOIN 활용!
+              `<h2>${title}</h2>${description}<p>by ${topic[0].name}</p>`, // JOIN 활용으로 by 추가!  
               ` <a href="/create">create</a>
                 <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
@@ -62,7 +62,7 @@ var app = http.createServer(function(request,response){
           });
         });
     } else if(pathname === '/create'){
-     db.query('SELECT * FROM topic', function(error, topics){ // 콜백함수의 형식(signature). sql의 결과가 topics에 담김
+     db.query('SELECT * FROM topic', function(error, topics){ 
         if(error) {
             throw error;
         }
@@ -78,9 +78,10 @@ var app = http.createServer(function(request,response){
         <p>
           <textarea name="description" placeholder="description"></textarea>
         </p>
-         <select name='author'> // 콤보 박스 생성. name 속성은 서버로 제출되는 값.
+         <p><select name='author'> // 콤보 박스 생성. name 속성은 서버로 제출되는 값.
           ${template.authorSelect(authors)}
          </select>
+         </p>
         <p>
           <input type="submit">
         </p>
@@ -94,12 +95,12 @@ var app = http.createServer(function(request,response){
           body = body + data;
       });
       request.on('end', function(){
-          var post = qs.parse(body)
-          db.query(`INSERT INTO topic (title, description, created, author_id VALUES(?,?, NOW(), ?)`, [post.title, post.description, 1], function(error, result){
+          var post = qs.parse(body) // query string parse but post method so invisible
+          db.query(`INSERT INTO topic (title, description, created, author_id VALUES(?,?, NOW(), ?)`, [post.title, post.description, post.author], function(error, result){ // authorSelect method에서 select 태그의 name 속성에 해당하는 값 작성
               if(error) {
                   throw error;
               }
-              response.writeHead(302, {Location: `/?id=${result.insertId}`}); // 방금 삽입한 행의 id 값 찾기
+              response.writeHead(302, {Location: `/?id=${result.insertId}`}); // 방금 삽입한 행의 id 값 찾기, sql insert 후 else 상세보기 페이지 이동
               response.end(); 
           });       
       });
@@ -111,7 +112,11 @@ var app = http.createServer(function(request,response){
         db.query(`SELECT * FROM TOPIC WHERE id=${queryData.id}`, function(error2, topic){ 
             if(error2) {
               throw error2; 
-              }
+              }  
+        db.query('SELECT * FROM author', function(error3, authors){
+             if(error3){
+                 throw error3;
+             }
         var title = 'Update';
         var list = template.list(topics);  
         var html = template.HTML(topic[0].title, list,
@@ -120,7 +125,11 @@ var app = http.createServer(function(request,response){
               <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
               <p>
                 <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
+              </p> 
+             <p><select name='author'> // 콤보 박스 수정. 수정 시에는 원래 저자가 선택 되어 있어야함 -> 현재 저자를 매개변수로 전달
+              ${template.authorSelect(authors, topic[0].author_id)}
+             </select>
+             </p>
               <p>
                 <input type="submit">
               </p>
@@ -129,7 +138,7 @@ var app = http.createServer(function(request,response){
           );
         response.writeHead(200);
         response.end(html); 
-     });
+        });
     } else if(pathname === '/update_process'){
       var body = '';
       request.on('data', function(data){
@@ -137,7 +146,7 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          db.query('UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?', [post.title, post.desciption, post.id], function(error, result){
+          db.query('UPDATE topic SET title=?, description=?, author_id=? WHERE id=?', [post.title, post.desciption, post.author, post.id], function(error, result){
             if(error) {
                 throw error ;
             }
