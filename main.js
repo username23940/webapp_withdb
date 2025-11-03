d2373c89cc4f39f2944e8c7952bf86b94230d835
LIM JANGBIN <great_jb2005@naver.com>
2025-11-03 23:42:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -41,6 +41,12 @@ var app = http.createServer(function(request,response){ // 요청이 들어오�
     else if(pathname === '/author/create_process'){
       author.create_process(request, response);
       }
+    else if(pathname === '/author/update_process'){
+      author.update_process(request, response);
+      }
+    else if(pathname === '/author/delete_process'){
+      author.delete_process(request, response);
+      }
     else {
       response.writeHead(404);
       response.end('Not found');
